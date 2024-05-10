d1175d1c20c87892c2cb40d38a897431038f7357
Andrew Aghoghovwia <androyt1@gmail.com>
2024-05-10 13:25:26 +0100

    test for button count click

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,5 +1,7 @@
 import { it, describe, expect } from "vitest";
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import App from "../App";
 
 describe("App", () => {
@@ -12,5 +14,12 @@ describe("App", () => {
         render(<App />);
         const learnMore = screen.getByText(/Click on the Vite and React logos to learn more/i);
         expect(learnMore).toBeInTheDocument();
+    });
+    it("Should update button count on click", async () => {
+        render(<App />);
+        const user = userEvent.setup();
+        const countButton = screen.getByRole("button");
+        await user.click(countButton);
+        expect(countButton).toHaveTextContent(/count is 1/i);
     });
 });
